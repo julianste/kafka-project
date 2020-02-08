cd09dcfb882500073d4bd4d9776376a558dc29ef
julian_steger <julian.j.steger@campus.tu-berlin.de>
2020-02-08 23:58:01 +0100

    initial commit

---
+++ consumers/faust_stream.py
@@ -0,0 +1,80 @@
+"""Defines trends calculations for stations"""
+import logging
+
+import faust
+from dataclasses import dataclass
+
+
+logger = logging.getLogger(__name__)
+
+
+# Faust will ingest records from Kafka in this format
+@dataclass
+class Station(faust.Record):
+    stop_id: int
+    direction_id: str
+    stop_name: str
+    station_name: str
+    station_descriptive_name: str
+    station_id: int
+    order: int
+    red: bool
+    blue: bool
+    green: bool
+
+
+# Faust will produce records to Kafka in this format
+@dataclass
+class TransformedStation(faust.Record):
+    station_id: int
+    station_name: str
+    order: int
+    line: str
+
+
+# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
+#   places it into a new topic with only the necessary information.
+app = faust.App("stations-stream", broker="kafka://localhost:9092;kafka://localhost:9093;kafka://localhost:9094", store="memory://")
+topic = app.topic("com.udacity.raw.stations", value_type=Station)
+# TODO: Define the output Kafka Topic
+out_topic = app.topic("com.udacity.clean.stations", partitions=1)
+# TODO: Define a Faust Table
+table = app.Table(
+   "stations-table",
+   default=TransformedStation,
+   partitions=1,
+   changelog_topic=out_topic,
+)
+
+
+#
+#
+# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
+# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
+# then you would set the `line` of the `TransformedStation` record to the string `"red"`
+#
+#
+@app.agent(topic)
+async def stations_event(stations):
+    async for station in stations:
+        line = "N/A"
+        if station.red:
+            line="red"
+        elif station.green:
+            line="green"
+        elif station.blue:
+            line="blue"
+        else:
+            raise Exception("no line provided for inamed " + str(station.station_name))
+
+        table[station.station_id] = TransformedStation(
+            station.station_id,
+            station.station_name, 
+            station.order, 
+            line
+        )
+        print(f"{station.station_id}: ${table[station.station_id]}")
+
+
+if __name__ == "__main__":
+    app.main()
